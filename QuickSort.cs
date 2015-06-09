ce8f343bce3ea1e1b698ba6f4c8bea62d1658de7
DanielBRGGA <danielg87.dg@gmail.com>
2015-06-08 18:35:57 -0600

    Metodo ordenamiento quick sort
    Clase en c sharp

---
+++ QuickSort.cs
@@ -0,0 +1,51 @@
+﻿
+namespace QuickSort
+{
+    class QuickSort
+    {
+
+        //declaramos un arreglo de enteros
+
+        private jugador[] vector;
+
+        public QuickSort(){}
+        public void ordenarVector(jugador[] vector, int primero, int ultimo)
+        {
+                int i,j, central;
+                int pivote;
+                central =(primero +ultimo)/2;//identificamos el elemento central del arreglo
+                pivote = vector[central].NumeroJugador;
+                i = primero;
+                j = ultimo;
+                do
+                {
+                    while (vector[i].NumeroJugador < pivote) i++;
+
+                    while (vector[j].NumeroJugador > pivote) j--;
+                    if (i <= j)
+                    {
+                        jugador temp;
+                        temp =vector[i];
+                        vector[i]=vector[j];
+                        vector[j]=temp;
+                        i++;
+                        j--;
+                    } 
+
+                } while (i <= j);
+
+                if (primero < j) ordenarVector(vector, primero, j);
+
+                if (i < ultimo) ordenarVector(vector, i, ultimo);
+               
+                this.vector = vector;
+
+               
+        }
+
+        public jugador[] devolverVector()
+        {
+            return vector;
+        }
+    }
+}
